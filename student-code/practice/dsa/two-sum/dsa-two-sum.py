9702fdae0448066ed4ef7b8ac67c780de538652f
Ahmed200444 <ahmedalkadi02@gmail.com>
2026-08-09 19:46:11 +0400

    Migrate Two Sum practice into its own folder

---
+++ student-code/practice/dsa/two-sum/dsa-two-sum.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target):
+    seen = {}
+
+    for index, number in enumerate(nums):
+        complement = target - number
+
+        if complement in seen:
+            return [seen[complement], index]
+
+        seen[number] = index
+
+    return None
